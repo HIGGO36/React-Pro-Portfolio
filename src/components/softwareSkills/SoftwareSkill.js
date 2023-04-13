c058ae93cb183a3599db9c3aafe29254348ae9dc
HIGGO36 <jwebdev99@protonmail.com>
2023-04-13 16:16:21 -0400

    adding dark-mode logic to the EducationCard and Software Skills JS Logic

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -5,10 +5,10 @@ import StyleContext from "../../contexts/StyleContext";
 
 export default function SoftwareSkill() {
   const { isDark } = useContext(StyleContext);
-
+  
   return (
-    <div className={isDark ? "dark-mode" : null}>
-      <div className="software-skills-main-div">
+    <div>
+      <div className={`software-skills-main-div ${isDark ? 'dark-mode' : ''}`}>
         <ul className="dev-icons">
           {skillsSection.softwareSkills.map((skills, i) => {
             return (
@@ -17,16 +17,8 @@ export default function SoftwareSkill() {
                 className="software-skill-inline"
                 name={skills.skillName}
               >
-                <i className={skills.fontAwesomeClassname}></i>
-                <p
-                  className={
-                    isDark
-                      ? "dark-mode software-skill-name"
-                      : "software-skill-name"
-                  }
-                >
-                  {skills.skillName}
-                </p>
+                <i className={`${skills.fontAwesomeClassname} ${isDark ? 'dark-mode' : ''}`}></i>
+                <p className={`${isDark ? 'dark-mode' : ''}`}>{skills.skillName}</p>
               </li>
             );
           })}
