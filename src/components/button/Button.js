6048e8a1da8b4bb75b7659cd280352a08b073ac1
HIGGO36 <jwebdev99@protonmail.com>
2023-04-13 00:44:09 -0400

    adding aria-labels to button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./Button.scss";
 
-export default function Button({text, className, href, newTab}) {
+export default function Button({text, className, href, newTab, ariaLabel, ariaDescription}) {
+  const buttonProps = {};
+
+  if (ariaLabel) {
+    buttonProps["aria-label"] = ariaLabel;
+  }
+
+  if (ariaDescription) {
+    buttonProps["aria-describedby"] = ariaDescription;
+  }
+
   return (
     <div className={className}>
-      <a className="main-button" href={href} target={newTab && "_blank"}>
+      <a className="main-button" href={href} target={newTab && "_blank"} {...buttonProps}>
         {text}
       </a>
     </div>
